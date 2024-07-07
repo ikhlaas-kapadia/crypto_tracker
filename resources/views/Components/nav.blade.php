08efe20e18e014139b45319d000b222e1214a314
ikhlaas <ikhlaas.ka@gmail.com>
2024-07-07 17:46:08 +0100

    resources/views/Components/coins.blade.php

--- resources/views/Components/nav.blade.php
+++ resources/views/Components/nav.blade.php
@@ -1,7 +1,6 @@
-@props(['message' => 'hello'])
-{{ $message }}
+
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
+  <a class="navbar-brand" href="/"><i class="fa-brands fa-btc" aria-hidden="true"></i></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -12,31 +11,27 @@
                 Home 
             </x-nav-link>
         </li>
+
         <li class="nav-item {{ request()->is('portfolio') ? 'active' : '' }}">
             <x-nav-link href="/portfolio">
                 Portfolio 
             </x-nav-link>
         </li>
-        <li class="nav-item {{ request()->is('profile') ? 'active' : '' }}">
-            <x-nav-link href="/profile">
-                My Profile 
-            </x-nav-link>
-        </li>
+        @if(! Auth::user())
         <li class="nav-item {{ request()->is('register') ? 'active' : '' }}">
-            <x-nav-link href="/register">
-                Register
-            </x-nav-link>
-        </li>
-        <li class="nav-item {{ request()->is('/crypto') ? 'active' : '' }}">
-            <x-nav-link href="/crypto">
-                Single Crypto
+            <x-nav-link href="{{route('register')}}" class="register-btn">
+               Register
             </x-nav-link>
         </li>
         <li class="nav-item {{ request()->is('login') ? 'active' : '' }}">
-            <x-nav-link href="/cryptocurrency">
+            <x-nav-link href="{{route('login')}}" class="login-link">
                 Login
             </x-nav-link>
         </li>
+        @endif
+
+        
     </ul>
   </div>
+
 </nav>
