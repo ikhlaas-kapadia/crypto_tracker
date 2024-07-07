c81aba4d9f8a3c97d99431ec7f7916bb6f0e4f4c
ikhlaas <ikhlaas.ka@gmail.com>
2024-07-07 17:45:35 +0100

    add data table styling

--- public/js/coins.js
+++ public/js/coins.js
@@ -1,5 +1,30 @@
-
 $(document).ready(function() {
-    console.log('hello');
-    $('#example').DataTable();
+    $('#coins-table').DataTable({
+        columnDefs: [
+            { "width": "5%", "targets": 0 },
+            { "width": "5%", "targets": 1 },
+            { "width": "10%", "targets": 2 },
+            { "width": "10%", "targets": 3 },
+            { "width": "20%", "targets": 4 },
+            { "width": "10%", "targets": 5 },
+            { "width": "10%", "targets": 6 },
+            { "width": "10%", "targets": 7 }
+        ],
+        responsive: true,
+        scroller: true,
+        scrollY: 400
+        
+    });
+
+    $('i.star').on('click', function(){
+        console.log('clicked');
+        if($(this).hasClass('far')){
+            $(this).removeClass('far').addClass('fas');
+            $(this).css('color', 'orange');
+        } else {
+            $(this).removeClass('fas').addClass('far');
+            $(this).css('color', 'black');
+        }
+        // fa fa-star fa-lg"
+    });
 });
